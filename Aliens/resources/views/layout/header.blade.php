f01fb65b479a8f2cc9c5422b17d4c5ae21d873f9
Gustavo Milczwski <gusmilczwski@gmail.com>
2017-11-28 13:25:29 -0200

    Add config files

--- Aliens/resources/views/layout/header.blade.php
+++ Aliens/resources/views/layout/header.blade.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link rel="stylesheet"  type="text/css"  href="../../../../css/bootstrap.css")}}">
     <title>
     	@section('title')
     	@show
